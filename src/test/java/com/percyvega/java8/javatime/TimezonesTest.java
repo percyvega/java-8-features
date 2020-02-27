66e08c77cf358970d8de05435302f71a9d210a50
percyvega <percyvega@gmail.com>
2020-02-27 00:01:10 -0500

    Added TimezonesTest

--- src/test/java/com/percyvega/java8/javatime/TimezonesTest.java
+++ src/test/java/com/percyvega/java8/javatime/TimezonesTest.java
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TimezonesTest {
 
     @Test
-    void timezonesTest() {
+    void zonedDateTimeTest() {
         ZonedDateTime zonedDateTime = ZonedDateTime.now();
         String localDateTimeString = zonedDateTime.toLocalDateTime().toString();
         String offsetString = zonedDateTime.getOffset().toString();
@@ -30,8 +30,28 @@ public class TimezonesTest {
     @Test
     void zoneIdTest() {
         assertThat(ZoneId.systemDefault().toString()).isEqualTo("America/New_York");
+        assertThat(ZoneId.systemDefault().getRules().getTransitions().size()).isEqualTo(177);
+
+        ZoneId zoneId = ZoneId.of("America/Los_Angeles");
+        assertThat(zoneId.toString()).isEqualTo("America/Los_Angeles");
+        assertThat(zoneId.normalized().toString()).isEqualTo("America/Los_Angeles");
+        assertThat(zoneId.getId()).isEqualTo("America/Los_Angeles");
 
         assertThat(ZoneId.getAvailableZoneIds().size()).isEqualTo(600);
+
+        ZonedDateTime ny = ZonedDateTime.now(ZoneId.of("America/New_York"));
+        ZonedDateTime la = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+
+        assertThat(ny.getHour()).isEqualTo((la.getHour() + 3) % 24);
+        assertThat(ny.getMinute()).isEqualTo(la.getMinute());
+        assertThat(ny.getSecond()).isEqualTo(la.getSecond());
+
+        LocalDateTime nyLDT = LocalDateTime.now(ZoneId.of("America/New_York"));
+        LocalDateTime laLDT = LocalDateTime.now(ZoneId.of("America/Los_Angeles"));
+
+        assertThat(nyLDT.getHour()).isEqualTo((laLDT.getHour() + 3) % 24);
+        assertThat(nyLDT.getMinute()).isEqualTo(laLDT.getMinute());
+        assertThat(nyLDT.getSecond()).isEqualTo(laLDT.getSecond());
     }
 
     @Test
